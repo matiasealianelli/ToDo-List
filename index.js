fdfab8b0f760065b54e8bcc6dc3993eec173f4c4
matiasealianelli <hola.matiasalianelli@gmail.com>
2025-05-30 23:40:16 -0300

    Agregue: Validaciones e iconos en los items + Agregue estilos CSS

--- index.js
+++ index.js
@@ -1,25 +1,51 @@
 let taskInput = document.querySelector(".taskInput")
 let btnAddTask = document.querySelector("#btn-add-task")
 let taskList = document.querySelector("#taskList")
-let mensaje = document.querySelector(".mensaje")
 let valueInput = taskInput.value
+let popUp = document.querySelector(".pop-up")
+
 //Funcion agregar item a la lista
 function agregarItem(e) {
-  let itemList = document.createElement("li")
-  let btnDelete = document.createElement("button")
-  itemList.innerHTML =  taskInput.value
-  taskList.appendChild(itemList)
-  itemList.appendChild(btnDelete)
+  if (taskInput.value != "") {
+    let conteinerItem = document.createElement("div")
+    conteinerItem.classList.add("conteiner-item-list")
+    let itemList = document.createElement("li")
+    itemList.classList.add("item-list")
+    let btnDelete = document.createElement("span")
+    btnDelete.classList.add("btn-delete")
+    let checkItem = document.createElement("span")
+    checkItem.classList.add("check-item")  
+    checkItem.innerHTML = `<i class="bi bi-check-circle-fill "></i>`
+    btnDelete.innerHTML = `<i class="bi bi-trash3 "></i>`
+    itemList.innerHTML = taskInput.value
+    // conteinerItem.appendChild(itemList)
+    conteinerItem.appendChild(checkItem)
+    checkItem.appendChild(itemList)
+    
+    conteinerItem.appendChild(btnDelete)
+    taskList.appendChild(conteinerItem)
+
     //Evento al hacer click sobre el item completado agrega estilos
-    itemList.addEventListener("click", () => {
-      itemList.classList.toggle("completed")
+    conteinerItem.addEventListener("click", () => {
+      conteinerItem.classList.toggle("completed")
     })
-    //Evento que elimina el item de la lista
-    btnDelete.addEventListener("click", () => {
-      itemList.remove()
-    })
+      //Evento que elimina el item de la lista
+  btnDelete.addEventListener("click", () => {
+    conteinerItem.remove()
+  })
   taskInput.value = ""
   taskInput.focus()
+  } else {
+    if (taskInput.value != "") {
+      return
+    } else {
+    popUp.innerHTML = "Por favor, ingresa una tarea antes de agregarla"
+    popUp.style.color= "red"
+    }
+  }
+  taskInput.addEventListener("keydown", (e) => {
+      popUp.innerHTML = ""
+  });
 }
 btnAddTask.addEventListener("click", agregarItem)
 taskInput.addEventListener("keypress", (e) => {
